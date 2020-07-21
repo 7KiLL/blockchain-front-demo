088ce323954646ff63d180d0033c5f5f5196cd84
7KiLL <7killua@gmail.com>
2020-07-21 20:34:11 +0300

    Fixed prod build with styles budget

--- src/app/modules/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/modules/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
+
 @Component({
   selector: 'app-dashboard-layout',
   templateUrl: './dashboard-layout.component.html',
-  styleUrls: ['./dashboard-layout.component.scss', './assets/sb-admin-2.min.css', '../../../../assets/fontawesome-free/css/all.min.css'],
-  encapsulation: ViewEncapsulation.ShadowDom
+  styleUrls: ['./dashboard-layout.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DashboardLayoutComponent implements OnInit {
 
