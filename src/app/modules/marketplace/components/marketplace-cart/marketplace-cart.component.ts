8f49980efa395d433183d0e143dc20e99bd0c108
Orest <orest.bilous@gmail.com>
2020-12-10 18:21:27 +0200

    customer fixes #4

--- src/app/modules/marketplace/components/marketplace-cart/marketplace-cart.component.ts
+++ src/app/modules/marketplace/components/marketplace-cart/marketplace-cart.component.ts
@@ -2,8 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {MarketplaceState} from '../../store/marketplace.reducer';
 import {selectMarketplaceCartState} from '../../store/marketplace.selectors';
-import {map, take} from 'rxjs/operators';
-import {Checkout, ClearCart} from '../../store/marketplace.actions';
+import {delay, map, take} from 'rxjs/operators';
+import {Checkout } from '../../store/marketplace.actions';
+import {PurchaseModalComponent} from '../../modals/purchase-modal/purchase-modal.component';
+import {MarketplaceEffects} from '../../store/marketplace.effects';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-marketplace-cart',
@@ -19,13 +22,45 @@ export class MarketplaceCartComponent implements OnInit {
     map(items => items.length)
   );
 
-  constructor(private store: Store<MarketplaceState>) {
+  constructor(
+    private store: Store<MarketplaceState>,
+    private marketplaceEffects: MarketplaceEffects,
+    private modalService: NgbModal,
+  ) {
   }
 
   ngOnInit(): void {
   }
 
   handleCheckout() {
-    this.store.dispatch(new Checkout());
+
+    const modalRef = this.modalService.open(PurchaseModalComponent, {
+      centered: true
+    });
+    // modalRef.componentInstance.fillModal($event);
+    this.marketplaceEffects.checkout$
+      /**
+       * We will take in only once since subscription will create every time onClick
+       */
+      .pipe(
+        take(1),
+        delay(250)
+      )
+      .subscribe(res => {
+        modalRef.close();
+    });
+
+    modalRef.componentInstance.checkout
+      /**
+       * Same as above example
+       */
+      .pipe(take(1))
+      .subscribe(item => {
+        this.store.dispatch(new Checkout());
+      });
   }
+  //
+  // handleCheckout() {
+  //   this.store.dispatch(new Checkout());
+  // }
 }
--- src/app/modules/marketplace/mock/mock-item.service.ts
+++ src/app/modules/marketplace/mock/mock-item.service.ts
@@ -7,49 +7,49 @@ import {delay} from 'rxjs/operators';
 export class MockItemService {
 
   private items: ItemModel[] = [{
-    src: 'https://images.unsplash.com/photo-1594046242234-52a439067587?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    src: 'https://images.unsplash.com/photo-1572804013309-59a88b7e92f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     title: 'aenean sit',
     price: 53,
     description: 'massa volutpat convallis morbi odio odio',
     store: 'Pierre Cardin'
   }, {
-    src: 'https://images.unsplash.com/photo-1594491084435-07c418f83735?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    src: 'https://images.unsplash.com/photo-1533228100845-08145b01de14?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     title: 'lobortis sapien sapien non mi',
     price: 14,
     description: 'diam id ornare imperdiet sapien urna pretium',
     store: 'Xiaomi'
   }, {
-    src: 'https://images.unsplash.com/photo-1594488506255-a8bbfdeedbaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    src: 'https://images.unsplash.com/photo-1602443276032-3dfd0f669041?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     title: 'at velit',
     price: 48,
     description: 'praesent blandit lacinia erat vestibulum sed magna at nunc commodo',
     store: 'Balmain'
   }, {
-    src: 'https://images.unsplash.com/photo-1594492415237-24f03f84c506?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    src: 'https://images.unsplash.com/photo-1544923246-77307dd654cb?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTV8fGphY2tldHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     title: 'pretium quis lectus',
     price: 91,
     description: 'fermentum justo nec condimentum neque sapien placerat',
     store: 'Balmain'
   }, {
-    src: 'https://images.unsplash.com/photo-1593791935086-75516605a5d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    src: 'https://images.unsplash.com/photo-1527189919029-aeb3d997547d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGRyZXNzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     title: 'amet nulla quisque arcu libero',
     price: 93,
     description: 'ultrices posuere cubilia curae donec pharetra magna vestibulum',
     store: 'Pierre Cardin'
   }, {
-    src: 'https://images.unsplash.com/photo-1593723447295-0c48ba723d3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    src: 'https://images.unsplash.com/photo-1532181408476-c4cacd791406?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHNtYXJ0cGhvbmV8ZW58MHwxfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     title: 'aliquam erat',
     price: 52,
     description: 'donec vitae nisi nam ultrices libero non mattis',
     store: 'Xiaomi'
   }, {
-    src: 'https://images.unsplash.com/photo-1593926365674-ac277753ab76?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    src: 'https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8ZHJlc3N8ZW58MHwxfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     title: 'turpis enim',
     price: 60,
     description: 'ridiculus mus etiam vel augue vestibulum',
     store: 'Pierre Cardin'
   }, {
-    src: 'https://images.unsplash.com/photo-1593543294918-ca3634e04cdb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    src: 'https://images.unsplash.com/photo-1589041169060-94c69f192442?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjB8fGRyZXNzfGVufDB8MXwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     title: 'ultrices posuere cubilia curae nulla',
     price: 34,
     description: 'quis tortor id nulla ultrices aliquet maecenas leo odio condimentum',
@@ -59,25 +59,25 @@ export class MockItemService {
     title: 'sit amet consectetuer',
     price: 43,
     description: 'habitasse platea dictumst maecenas ut',
-    store: 'Pierre Cardin'
+    store: 'Starbucks'
   }, {
-    src: 'https://images.unsplash.com/photo-1567769541695-3598ee6ea57b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    src: 'https://images.unsplash.com/photo-1549399542-7e3f8b79c341?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FyfGVufDB8MXwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=600',
     title: 'in faucibus orci luctus et',
     price: 98,
     description: 'vel sem sed sagittis nam congue risus semper porta volutpat',
-    store: 'Balmain'
+    store: 'BMW'
   }, {
-    src: 'https://images.unsplash.com/photo-1593114970350-a491e24c0d7f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    src: 'https://images.unsplash.com/photo-1511920170033-f8396924c348?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Y29mZmVlfGVufDB8MXwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     title: 'eu magna vulputate',
     price: 81,
     description: 'ac leo pellentesque ultrices mattis odio donec vitae',
-    store: 'Xiaomi'
+    store: 'Lavazza'
   }, {
-    src: 'https://images.unsplash.com/photo-1593089762614-705b8fa9b875?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    src: 'https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8MXwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
     title: 'maecenas tristique',
     price: 49,
     description: 'consequat varius integer ac leo pellentesque ultrices mattis odio donec',
-    store: 'Pierre Cardin'
+    store: 'Apple'
   }];
 
   constructor() {
