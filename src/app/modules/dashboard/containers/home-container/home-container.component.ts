fb99f907991ea78cece2d37c118f3eac16b34503
Orest <orest.bilous@gmail.com>
2020-12-11 17:47:37 +0200

    pie chart removed, listing added #7

--- src/app/modules/dashboard/containers/home-container/home-container.component.ts
+++ src/app/modules/dashboard/containers/home-container/home-container.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {IAppState} from '../../../../core/store/app.state';
 import {
@@ -9,14 +9,14 @@ import {Observable, Subscription} from 'rxjs';
 import {buffer, bufferTime, filter, map, mergeAll, scan, tap} from 'rxjs/operators';
 import {ChartJsService} from '../../services/chart-js.service';
 import {ItemModel} from '../../../marketplace/models/item.model';
-import {flatten} from "@angular/compiler";
+import {flatten} from '@angular/compiler';
 
 @Component({
   selector: 'app-home-container',
   templateUrl: './home-container.component.html',
   styleUrls: ['./home-container.component.scss']
 })
-export class HomeContainerComponent implements OnInit, AfterViewInit, OnDestroy {
+export class HomeContainerComponent implements AfterViewInit, OnDestroy, OnInit {
 
   @ViewChild('canvasElement') pieChart: ElementRef;
 
@@ -24,34 +24,52 @@ export class HomeContainerComponent implements OnInit, AfterViewInit, OnDestroy 
     map(carts => carts.map(cart => cart.items)),
     // @ts-ignore IDE doesnt recognize new ES method
     map(items => items.flat()),
-
   );
   public walletBalance$ = this.store.select(selectMarketplaceWalletState).pipe(map(wallet => wallet.balance));
 
-
   private subscription = new Subscription();
 
-  constructor(private store: Store<IAppState>, private chartJsService: ChartJsService) {
+  private listingTitles = ['Personal wallet', 'Advertising', 'Development', 'Xiaomi', 'Balman', 'Pierre Cardin'];
+
+  public listingObjects: {title: string, value: number}[];
+
+  constructor(
+    private store: Store<IAppState>,
+    private chartJsService: ChartJsService,
+    ) {
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit() {
+    this.subscription = this.store.select(selectMarketplaceWalletState).subscribe(wallet => {
+      this.mapListing(wallet);
+    });
+    }
 
   ngAfterViewInit() {
-    this.chartSetup();
-    this.subscription = this.store.select(selectMarketplaceWalletState).subscribe(wallet => {
-      this.chartJsService.refillChart(wallet);
-    });
+    // this.chartSetup();
+    // this.subscription = this.store.select(selectMarketplaceWalletState).subscribe(wallet => {
+      // this.chartJsService.refillChart(wallet);
+    // });
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
-  private chartSetup() {
-    const ctx = this.pieChart.nativeElement;
-    this.chartJsService.setupInstance(ctx);
+  // private chartSetup() {
+  //   const ctx = this.pieChart.nativeElement;
+  //   this.chartJsService.setupInstance(ctx);
+  // }
+
+  private mapListing(wallet) {
+    const values = [wallet.sub1, wallet.sub2, wallet.sub3, wallet.sub4, wallet.sub5, wallet.sub6];
+    const mappedArray = this.listingTitles.map((title, index) => {
+      return {
+        title,
+        value: values[index].toFixed(2),
+      };
+    });
+    this.listingObjects = mappedArray.filter(item => item.value > 0);
   }
 
 }
